0dab487bddd63cd198eb365ce794b96fcc11333c
Andrew Hao <andrewhao@gmail.com>
2016-02-08 22:12:30 -0800

    Fixing npm issues

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack')
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: {
-		"quickCadence": "./lib/quickCadence",
+    "quickCadence": "./lib/quickCadence",
     "referenceRunner": "./reference/js/runner",
 	},
   output: {
