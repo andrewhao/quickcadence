40712436e13e8e855c644438674a2984fc25e013
Andrew Hao <andrewhao@gmail.com>
2016-02-21 21:05:13 -0800

    Boost test cases with all the files.

--- test/quickCadenceSpec.js
+++ test/quickCadenceSpec.js
@@ -4,14 +4,30 @@ var QuickCadence = require('../lib/quickCadence'),
     assert = require('assert');
 
 describe('QuickCadence', function() {
-  describe('.pipe()', function() {
-    it('takes a sample set of data and returns a set of cadence details', function(done) {
-      this.timeout(10000);
+  [
+    ['samples-1.csv', 74],
+    ['samples-2.csv', 72],
+    ['samples-3.csv', 79],
+    ['samples-4.csv', 76],
+    ['samples-5.csv', 68]
+  ].forEach((testCase) => {
+    describe('.pipe()', function() {
+      const [fileName, expectedValue] = testCase;
+      it('takes a sample set of data and returns a set of cadence details', function(done) {
+        this.timeout(7000);
 
-      var points = fs.readFileSync(__dirname + '/sample.csv', 'utf-8');
-      var stream = TestDataStream.pointsAsStream(points);
-      var cadenceStream = QuickCadence.pipe(stream)
-      cadenceStream.onValue(function(v) { console.log(v); done(); })
+        var points = fs.readFileSync(`${__dirname}/../data/${fileName}`, 'utf-8');
+        var stream = TestDataStream.pointsAsStream(points);
+        var cadenceStream = QuickCadence.pipe(stream)
+        cadenceStream
+        .firstToPromise(Promise)
+        .then((v) => {
+          console.log(`Expected: ${expectedValue}`);
+          console.log(`Actual: ${v}`);
+          assert(Math.abs(v - expectedValue) < 2);
+          done();
+        })
+      });
     });
   });
 });
