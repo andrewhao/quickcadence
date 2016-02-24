0c5dea864ca82b0fb5762dbbd8f3fe396697b527
Andrew Hao <andrewhao@gmail.com>
2016-02-24 07:49:42 -0800

    Version bump - publish to NPM. RxCadence is now main entry point.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: {
     "QuickCadence": ["./lib/QuickCadence"],
+    "RxCadence": ["./lib/RxCadence"],
     "BaconReferenceRunner": ["./reference/js/BaconRunner"],
     "RxJSReferenceRunner": ["./reference/js/RxJSRunner"],
 	},
